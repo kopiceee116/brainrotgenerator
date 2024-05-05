f7dc8f5c6a070e3d870b0082673efe1263201c7e
Köbe Koppány <kkoppany06@gmail.com>
2024-05-05 19:08:59 +0200

    van idozites

--- idozito.py
+++ idozito.py
@@ -1,5 +1,6 @@
 from moviepy.editor import *
 import wave, contextlib, os
+import random #to have random starting point in video :)
 
 def VideoLength(videofilepath):
     bigVideo = VideoFileClip(videofilepath)
@@ -11,7 +12,7 @@ def AudioLength(filename):
         frames = f.getnframes()
         rate = f.getframerate()
         duration = frames / float(rate)
-        return round(duration+0.02,2)
+        return round(duration+0.1,1)
 
 def totalAuidoLength(folder :str) -> float:
     TotalTimeTalking = 0
@@ -19,15 +20,25 @@ def totalAuidoLength(folder :str) -> float:
         path = os.path.join(folder,file)
         TotalTimeTalking+=AudioLength(path)
 
-    return round(TotalTimeTalking,2)
-
-print(totalAuidoLength("hang"))
+    return round(TotalTimeTalking,1)
 
 def findStartingPoint(videoPath :str,audioFolderPath :str) -> int:
-    return round(VideoLength(videoPath) - totalAuidoLength(audioFolderPath) - 10)
+    return 100
+    return random.randint(1,round(VideoLength(videoPath) - totalAuidoLength(audioFolderPath) - 10))
 
-print(findStartingPoint('BackgroundVid.mp4','hang'))
+def startandEndLengths(videoPath,audioFolderPath):
+    startingPoint = findStartingPoint(videoPath,audioFolderPath)
+    StartandEndPoints = []
+    print(startingPoint)    
+    for file in os.listdir(audioFolderPath): 
+        path = os.path.join(audioFolderPath,file)
+        endpoint = round(startingPoint+AudioLength(path),1)
+        StartandEndPoints.append((startingPoint,endpoint))
+        startingPoint = round(endpoint+0.1, 1)
+    print(StartandEndPoints)
+        
 
+print(startandEndLengths('BackgroundVid.mp4','hang'))    
 
 # Make the text. Many more options are available.
 # txt_clip = ( TextClip("My Holidays 2013",fontsize=70,color='white')
